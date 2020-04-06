7a6825839a7266727fdfcaacd3dd5ce777247b29
Chris Eld <chris.eld95@gmail.com>
2020-04-05 22:02:52 -0400

    finished app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fitness-tracker
+A simple fitness tracking application
+
+![Screenshot of the website](https://github.com/chriseld/portfolio/blob/master/assets/img/fitness-tracker.png)
+
+## About
+
+A simple fitness tracker that makes use of a mongoDB database
+
+## Deployment
+
+The page repository can be found at [the GitHub page](https://github.com/chriseld/fitness-tracker).
+The deployed version of the page can be found at [GitHub Pages](https://chriseld.github.io/portfolio/).
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", { useNewUrlParser: true, useFindAndModify: false });
+
+(require("./controllers/html-routes.js"))(app);
+(require("./controllers/api-routes.js"))(app);
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
